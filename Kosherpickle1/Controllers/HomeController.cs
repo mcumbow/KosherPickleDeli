ccc4dacb87e31800bc630501202cb89ae1b00ef6
Michael Cumbow <mcumbow@comcast.net>
2021-05-07 02:31:46 -0400

    Add project files.

---
+++ Kosherpickle1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kosherpickle1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            ViewBag.Message = "The Menu.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Kosher Pickle Deli Contact Information.";
+
+            return View();
+        }
+    }
+}
